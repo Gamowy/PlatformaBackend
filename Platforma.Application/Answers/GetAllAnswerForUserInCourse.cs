3f5d877cf8cf8c4ea099ba7c6749ab75b7b33fe3
Aleksandra Starowicz <as307323@student.polsl.pl>
2025-06-09 18:01:01 +0200

    Dodanie endpointów do pobrania tylko zaakceptowanych uczestników kursu oraz wszytskich odpowiedzi dla jednego użytkownika dla danego kursu

---
+++ Platforma.Application/Answers/GetAllAnswerForUserInCourse.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Platforma.Application.Courses.DTOs;
+using Platforma.Domain;
+using Platforma.Infrastructure;
+
+namespace Platforma.Application.Answers
+{
+    public class GetAllAnswerForUserInCourse
+    {
+        public class Query : IRequest<Result<List<Answer>>>
+        {
+            public required Guid CourseId { get; set; }
+            public required Guid UserId { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, Result<List<Answer>>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Result<List<Answer>>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var course = await _context.Courses.FindAsync(request.CourseId);
+                if (course == null) return Result<List<Answer>>.Failure("Course not found");
+
+
+                var answers = await _context.Answers
+                    .Where(a => _context.Assignments
+                        .Where(x => x.CourseId == request.CourseId)
+                        .Select(x => x.Id)
+                        .Contains(a.AssignmentId)
+                        && a.UserId == request.UserId)
+                    .ToListAsync(cancellationToken);
+
+                if (answers == null || answers.Count == 0) return Result<List<Answer>>.Failure("No answers found for this user in this course");
+
+                return Result<List<Answer>>.Success(answers);
+
+            }
+        }
+    }
+}
