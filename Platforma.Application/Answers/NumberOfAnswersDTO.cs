0831241290d16599947376b0312741c03bb26640
Patryk Gamrat <Patryk.gam@gmail.com>
2025-06-15 03:32:19 +0200

    Add endpoint to get number of answers for each assignment in course

---
+++ Platforma.Application/Answers/NumberOfAnswersDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Platforma.Application.Answers
+{
+    public class NumberOfAnswersDTO
+    {
+        public Guid AssignmentId { get; set; }
+        public int UnmarkedAnswers { get; set; }
+        public int MarkedAnswers { get; set; }
+    }
+}
